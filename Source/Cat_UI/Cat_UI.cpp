67fe5d7086f1ab371c22cdbf8e8a5d04c037f21c
PERARD-G N <perard.git@outlook.com>
2019-11-14 10:01:28 +0000

    fixed startup messages

--- Source/Cat_UI/Cat_UI.cpp
+++ Source/Cat_UI/Cat_UI.cpp
@@ -9,14 +9,14 @@ DEFINE_LOG_CATEGORY(Log_Cat_UI);
 void FCat_UI::StartupModule()
 {
 #if WITH_EDITOR
-	UE_LOG(Log_Cat_UI, Warning, TEXT("WH_Dialogues module has started!"));
+	UE_LOG(Log_Cat_UI, Warning, TEXT("Cat UI module has started!"));
 #endif //WITH_EDITOR
 }
 
 void FCat_UI::ShutdownModule()
 {
 #if WITH_EDITOR
-	UE_LOG(Log_Cat_UI, Warning, TEXT("WH_Dialogues module has shut down"));
+	UE_LOG(Log_Cat_UI, Warning, TEXT("Cat UI module has shut down"));
 #endif //WITH_EDITOR
 }
 
