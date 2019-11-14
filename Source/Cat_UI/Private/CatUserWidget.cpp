e31f6c6233252a653f8bd9ca86d1e603b953816f
PERARD-G N <perard.git@outlook.com>
2019-11-14 09:56:25 +0000

    added static helpers for widgets

--- Source/Cat_UI/Private/CatUserWidget.cpp
+++ Source/Cat_UI/Private/CatUserWidget.cpp
@@ -1,3 +1,39 @@
 // Noe Perard-Gayot <noe.perard@gmail.com> 2019 - All Rights Reserved
 
 #include "CatUserWidget.h"
+
+
+
+template<typename WidgetT, typename OwnerT>
+WidgetT * UCatUserWidget::CreateWidget(OwnerT * owner, TSubclassOf< UCatUserWidget > userWidgetClass)
+{
+    //  UserWidgetClass.GetName() 
+    FName newwidgetname = MakeUniqueObjectName(owner, UserWidgetClass, UserWidgetClass.GetDefaultObjectName());
+    return CreateWidget<WidgetT,OwnerT>(owner, UCatUserWidget, newwidgetname);
+}
+
+template<typename WidgetCompT>
+static WidgetCompT * UCatUserWidget::CreateWidgetComponent(AActor * owner, TSubclassOf< UCatUserWidget > userWidgetClass, TSubclassOf< WidgetCompT > componentClass, bool worldSpace)
+{
+    if(!userWidgetClass.Get() || !owner || !WidgetCompT->IsChildOf<UWidgetComponent>())
+        return nullptr;
+    
+    FName newwidgetname = MakeUniqueObjectName(owner, componentClass, userWidgetClass.GetDefaultObjectName());
+    WidgetCompT* newcomp = NewObject<WidgetCompT>( ComponentClass, this, YourObjectName);
+
+    if(!newcomp)
+        return nullptr;
+
+    const auto widget = CreateWidget(newcomp,UserWidgetClass);
+
+    widgetcomp = static_cast<UWidgetComponent*>(newcomp);
+    widgetcomp->RegisterComponent();        //You must ConstructObject with a valid Outer that has world, see above	 
+    widgetcomp->SetWorldLocation(Location); 
+    widgetcomp->SetWorldRotation(Rotation); 
+    widgetcomp->AttachTo(GetRootComponent(),SocketName,EAttachLocation::KeepWorldPosition); 
+    widgetcomp->SetWidget(widget);
+    widgetcomp->SetWidgetSpace(worldSpace ? EWidgetSpace::World : EWidgetSpace::Screen);
+
+    return newcomp;     
+}
+    
--- Source/Cat_UI/Public/CatUserWidget.h
+++ Source/Cat_UI/Public/CatUserWidget.h
@@ -5,12 +5,23 @@
 #include "Blueprint/UserWidget.h"
 #include "CatUserWidget.generated.h"
 
+class AActor;
+class UWidgetComponent;
+
 /// @brief UCatUserWidget		base class for user widget
-UCLASS(ClassGroup=(PlayerController))
+UCLASS(ClassGroup=(UI))
 class CAT_UI_API UCatUserWidget : public UUserWidet
 {
 	GENERATED_BODY()
 public:
+
+    template<typename WidgetT, typename OwnerT>
+    static WidgetT * CreateWidget(OwnerT * Owner, TSubclassOf< UCatUserWidget > UserWidgetClass);
+
+
+    template<typename WidgetCompT>
+    static WidgetCompT * CreateWidgetComponent(AActor * Owner, TSubclassOf< UCatUserWidget > UserWidgetClass, TSubclassOf< WidgetCompT > ComponentClass, bool worldSpace = false);
+
 
 
 };
