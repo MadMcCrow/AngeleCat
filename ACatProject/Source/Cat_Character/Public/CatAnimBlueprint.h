5a977b44a479d077b8f90609c5add4b3255dfdde
MadMcCrow <noe.perard@outlook.com>
2019-12-07 19:27:00 +0100

    Moved folder tree to have a non-project directory as root

---
+++ ACatProject/Source/Cat_Character/Public/CatAnimBlueprint.h
@@ -0,0 +1,17 @@
+// Noe Perard-Gayot <noe.perard@gmail.com> 2019 - All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimBlueprint.h"
+#include "CatAnimBlueprint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAT_CHARACTER_API UCatAnimBlueprint : public UAnimBlueprint
+{
+	GENERATED_BODY()
+	
+};
---
+++ ACatProject/Source/Cat_Character/Public/CatMovementComponent.h
@@ -0,0 +1,17 @@
+// Noe Perard-Gayot <noe.perard@gmail.com> 2019 - All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "CatMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAT_CHARACTER_API UCatMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+};
